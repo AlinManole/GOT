3a295c2030660daf5e30dc1346239c01529fe3eb
Alin Manole <alin.manole@yahoo.com>
2021-10-20 10:06:05 +0200

    push 10.06

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 $(function () {
+    var array = []
     $.ajax({
         url: 'https://thronesapi.com/api/v2/Characters',
         success: function (data) {
-            console.log(data)
+            array = data;
+            console.log(array)
+            array.forEach(function (personaje, index) {
+                console.log(personaje.firstName)
+                $("#container").html($("#container").html() + `
+                    <div class="include col-3 m-1">
+                        <div class="box">
+                            <img class="theImage" src="${personaje.imageUrl}" alt="" />
+                            <div class = "textBox">
+                                <h3>${personaje.fullName}</h3>
+                                <h5>${personaje.title}</h5>
+                            </div>
+                        </div>
+                    </div>
+                ` )
+            })
         }
     });
-})
-
+});
